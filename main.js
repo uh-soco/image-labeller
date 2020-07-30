78ccb57ae72a502ac53ec880d81d0ae2daa51026
kriskrok <25787775+kriskrok@users.noreply.github.com>
2020-07-30 12:21:44 +0300

    Introduces electron-builder. Thus far only package-linux.
    - Yanks NODE_ENV = dev definition from webpack.config.js to package.json
    - Electron builder linux targets only AppImage
    - Packaged software resides in /dist
    - TODO: yet to implement packaging for macOS and Windows
    - package.json -> build -> directories -> buildResources shall host icons in the future
    - devtools extension are installed only if NODE_ENV is set to dev, see main.js
    - updates .gitignore to exclude build and dist folders

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-const { default: installExtension, REDUX_DEVTOOLS } = require('electron-devtools-installer');
 const { BrowserWindow, app } = require('electron')
 
-
 const installExtensions = async () => {
+  const { default: installExtension, REDUX_DEVTOOLS } = require('electron-devtools-installer');
   const installer = require('electron-devtools-installer');
   const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
   const extensions = ['REACT_DEVELOPER_TOOLS', 'REDUX_DEVTOOLS'];
@@ -15,42 +14,26 @@ const installExtensions = async () => {
 const createWindow = async () => {
 
 
-  await installExtensions()
-  
   let win = new BrowserWindow({
         width: 1200,
         height: 600
     })
     
   if (process.env.NODE_ENV === 'dev') {
-    console.log('Käynnistetään kehitysympäristössäMME')
+    await installExtensions()
     win.loadURL('http://localhost:3003') // KehitykseMME
+
   } else {
     win.loadFile('./build/index.html') // TuotantoMME
   }
-
-  return dev
   
 }
 
-
-
-
-
-
-
-
 app.on('ready', () => {
-
-  console.log('ymparistoMME', process.env.NODE_ENV)
-  
+  if (process.env.NODE_ENV) {
+    console.log('ymparistoMME', process.env.NODE_ENV)
+  } 
+ 
   createWindow() // Chrome-kehitystyokalutMME
     //win.webContents.openDevTools()  )
-
 })
-
-
-
-
-
-
